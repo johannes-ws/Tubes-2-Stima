3ad1d6636e7b56d28b8ef9a49ce25ddac42d9e22
dxt99 <ericlouis19x@gmail.com>
2022-03-13 00:54:25 +0700

    Finished Input GUI

--- WindowsFormsApp1/Forms.cs
+++ WindowsFormsApp1/Forms.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp
@@ -14,10 +15,19 @@ namespace WindowsFormsApp
 
 		private TextBox textBox1;
 
-		private string RootFolder;
         private CheckBox checkBox1;
         private ComboBox comboBox1;
-        private string FileName;
+        private Label label3;
+        private Label Input;
+        private Label label4;
+        private Label label5;
+
+
+        // Inputs
+        public string RootFolder = "";
+        public string FileName = "";
+        public Boolean AllOccurance;
+        public string SearchType;
 
 		public Window()
 		{
@@ -35,22 +45,26 @@ namespace WindowsFormsApp
             this.label2 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Input = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.AcceptsReturn = true;
             this.textBox1.AcceptsTab = true;
-            this.textBox1.Location = new System.Drawing.Point(43, 137);
+            this.textBox1.Location = new System.Drawing.Point(46, 205);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(214, 31);
+            this.textBox1.Size = new System.Drawing.Size(214, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(46, 273);
+            this.button1.Location = new System.Drawing.Point(46, 361);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -60,7 +74,7 @@ namespace WindowsFormsApp
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(43, 70);
+            this.button2.Location = new System.Drawing.Point(46, 131);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(123, 23);
             this.button2.TabIndex = 5;
@@ -71,7 +85,7 @@ namespace WindowsFormsApp
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 47);
+            this.label1.Location = new System.Drawing.Point(43, 98);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(116, 17);
             this.label1.TabIndex = 6;
@@ -80,7 +94,8 @@ namespace WindowsFormsApp
             // labelFolder
             // 
             this.labelFolder.AutoSize = true;
-            this.labelFolder.Location = new System.Drawing.Point(172, 73);
+            this.labelFolder.Location = new System.Drawing.Point(175, 134);
+            this.labelFolder.MaximumSize = new System.Drawing.Size(250, 100);
             this.labelFolder.Name = "labelFolder";
             this.labelFolder.Size = new System.Drawing.Size(111, 17);
             this.labelFolder.TabIndex = 7;
@@ -90,11 +105,12 @@ namespace WindowsFormsApp
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(43, 108);
+            this.label2.Location = new System.Drawing.Point(43, 185);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(121, 17);
             this.label2.TabIndex = 8;
             this.label2.Text = "Nama File Target:";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
             // 
             // checkBox1
             // 
@@ -102,12 +118,13 @@ namespace WindowsFormsApp
             this.checkBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.checkBox1.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.checkBox1.Location = new System.Drawing.Point(46, 193);
+            this.checkBox1.Location = new System.Drawing.Point(46, 249);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(181, 21);
             this.checkBox1.TabIndex = 9;
             this.checkBox1.Text = "Cari Semua Kemunculan";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // comboBox1
             // 
@@ -115,15 +132,58 @@ namespace WindowsFormsApp
             this.comboBox1.Items.AddRange(new object[] {
             "DFS",
             "BFS"});
-            this.comboBox1.Location = new System.Drawing.Point(46, 229);
+            this.comboBox1.Location = new System.Drawing.Point(46, 311);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // Program
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(400, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(2, 451);
+            this.label3.TabIndex = 0;
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // Input
+            // 
+            this.Input.AutoSize = true;
+            this.Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Input.Location = new System.Drawing.Point(41, 43);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(70, 29);
+            this.Input.TabIndex = 11;
+            this.Input.Text = "Input";
+            this.Input.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 364);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Metode Pencarian:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Window
             // 
             this.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.ClientSize = new System.Drawing.Size(823, 447);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label2);
@@ -132,7 +192,7 @@ namespace WindowsFormsApp
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Name = "Program";
+            this.Name = "Window";
             this.Text = "File Crawler";
             this.Load += new System.EventHandler(this.Program_Load);
             this.ResumeLayout(false);
@@ -143,7 +203,8 @@ namespace WindowsFormsApp
 
 		private void textBox1_TextChanged(object sender, EventArgs e)
 		{
-
+            TextBox objTextBox = (TextBox)sender;
+            this.FileName = objTextBox.Text;
 		}
 
 		private void Program_Load(object sender, EventArgs e)
@@ -186,6 +247,45 @@ namespace WindowsFormsApp
 		}
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.RootFolder.Length == 0) this.label4.Text = "Folder Root Belum Dipilih!";
+            else if (this.FileName.Length == 0) this.label4.Text = "File Target Belum Diisi!";
+            else if (this.SearchType != "DFS" && this.SearchType != "BFS") this.label4.Text = "Metode Pencarian Belum Dipilih!";
+            else
+            {
+                //CALL FUNCTION
+                this.label4.Text = "Function Call";
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox objComboBox = (ComboBox)sender;
+            this.SearchType = objComboBox.Text;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox objCheckBox = (CheckBox)sender;
+            this.AllOccurance = objCheckBox.Checked;
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
         {
 
         }
